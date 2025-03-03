296d1679ed34e9d3dd8fabc87f44c92e2cdc7487
neerp03 <157861342+neerp03@users.noreply.github.com>
2025-03-02 20:31:25 -0500

    Update Piece.java

--- src/chess/Piece.java
+++ src/chess/Piece.java
@@ -1,5 +1,32 @@
 package chess;
 
-public abstract class Piece {
+import java.util.ArrayList;
 
+public abstract class Piece {
+    protected Color color;
+    protected Position position;
+    
+    public Piece(Color color, Position position) {
+        this.color = color;
+        this.position = position;
+    }
+    
+    public Color getColor() {
+        return color;
+    }
+    
+    public Position getPosition() {
+        return position;
+    }
+    
+    public void setPosition(Position pos) {
+        this.position = pos;
+    }
+    
+     
+    public abstract ArrayList<Position> getLegalMoves(Board board);
+    
+        
+    public abstract Piece copy();
 }
+
