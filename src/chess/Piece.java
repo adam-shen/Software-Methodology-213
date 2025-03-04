8e51ce1917fccc7820f49562851a7a61b111fe4c
neerp03 <157861342+neerp03@users.noreply.github.com>
2025-03-03 20:34:11 -0500

    Update Piece.java

--- src/chess/Piece.java
+++ src/chess/Piece.java
@@ -5,31 +5,34 @@ import java.util.ArrayList;
 public abstract class Piece implements Cloneable {
     protected Color color;
     protected Position position;
-
-    public Piece(Position position, Color color) {
+    
+    public Piece(Color color, Position position) {
         this.color = color;
         this.position = position;
     }
-
+    
     public Color getColor() {
         return color;
     }
-
+    
     public Position getPosition() {
         return position;
     }
-
+    
     public void setPosition(Position pos) {
         this.position = pos;
     }
-
+    
+     //Returns a list of legal moves based solely on the piece's movement rules.
+     
     public abstract ArrayList<Position> getLegalMoves(Board board);
-
+    
+    
+     //Returns a deep copy of the piece.
+    
     public abstract Piece copy();
-
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
+    
+    public Object clone() throws CloneNotSupportedException {
         return super.clone();
     }
-
 }
